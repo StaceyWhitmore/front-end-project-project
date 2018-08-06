e86a0b8ad8b2d767ecc5374fc632aacdd14e3d3e
StaceyWhitmore <staceywhitmore@gmail.com>
2018-08-06 09:43:09 -0600

    wrong file names. Refactored dirs. What was 'tile' has been replaced with 'article'

--- pages/index.js
+++ pages/index.js
@@ -12,10 +12,7 @@ function getPosts() {
 
   ]
 }
-//NEW
-/*
-Now let's add a simple change to our home page.
-We have isolated (outside of export statement) our link component like this:*/
+
 const PostLink = ({ post }) => (
   <li>
     <Link as={`/p/${post.id}`} href={`/post?title=${post.title}`}>
@@ -53,34 +50,7 @@ const PostLink = ({ post }) => (
       `}</style>
   </li>
 )
-/*
-REMOVED fr li {} styles:   ---->
 
- list-style: none;
-  margin: 5px 0;
-
-*/
-/*
-CSS rules have NO effect on ELEMENTS (i.e. <a>) [from?] INside of a Child COMPonent
-(from inside the <Layout> component. so create a new comp [e.g. PostLink] and
-style from within there instead. {
-  [However there is  a WORKAROUND by using the 'global' keyword after 'jsx'
-. But try to use this sparingly. It comes in handy with markdown text.
-To see this in action check out the pages.js module.]
-}).
-
-(i.e. Style them from within the parent component while declaring it and
-RATHER than waiting to style it from with the child component while exporting it
-using the export default () => statement.)
-
-This feature of styled-jsx helps you to manage styles for bigger apps.
-
-In that case, you need to style the child component DIRECTLY
-(i.e. From withIN the component const declaration (above) and NOT inside then
-export statement).
-And in our particular case, we need to do this for our Link component
-(style the Link component Directly):
-*/
 export default () => (
   <Layout>
     <h1>Front End React Project</h1>
@@ -92,23 +62,3 @@ export default () => (
 
   </Layout>
 )
-/*
-h1, a {
-  font-family: "Arial";
-}
-
-ul {
-  padding: 0;
-}
-*/
-
-/*
-Styles should go inside template strings
-
-Styled jsx works as a babel plugin.
-It will parse all of the CSS and apply it in the build process.
- (With that our styles get applied without any overhead time)
-
-It also supports having constraints inside styled-jsx.
-In the future, you will be able to use any dynamic variable inside styled-jsx.
-That is why CSS needs to go inside of a template string. ({``})*/
